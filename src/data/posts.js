2d0d975f9d25674bc921750a2d399fcd741f797e
spymethod <spymethod254@gmail.com>
2026-07-17 20:50:56 +0300

    Create src/data/posts.js

---
+++ src/data/posts.js
@@ -0,0 +1,29 @@
+export const posts = [
+  {
+    id: 1,
+    username: "Bakari",
+    content: "🚀 Welcome to LINKUP.HUB!",
+    likes: 25M,
+    comments: 3M,
+    shares: 44k,
+    time: "9 minutes ago",
+  },
+  {
+    id: 2,
+    username: "Bakari",
+    content: "Building my own social media platform 😄",
+    likes: 7M,
+    comments: 66k,
+    shares: 200k,
+    time: "10 minutes ago",
+  },
+  {
+    id: 3,
+    username: "Bakari",
+    content: "React + Supabase = ❤️",
+    likes: 5M,
+    comments: 882k,
+    shares: 3M,
+    time: "30 minutes ago",
+  },
+];
